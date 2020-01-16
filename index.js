8cef049b2872a09875057a61d301245b067f03e9
Angel Hurtado <angelxehg@outlook.com>
2020-01-16 16:12:21 -0600

    Check DB connection

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 'use strict';
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://heroku_qqw6mhz5:7o0ug689i04qd2c7mdtdj1luf3@ds331568.mlab.com:31568/heroku_qqw6mhz5'), (err, res) => {
-    if (err) {
-        throw err;
-    } else {
-        console.log("Connected to MongoDB");
-    }
-}
+mongoose.connect('mongodb://heroku_qqw6mhz5:7o0ug689i04qd2c7mdtdj1luf3@ds331568.mlab.com:31568/heroku_qqw6mhz5', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => console.log('DB Connected!'))
+    .catch(err => {
+        console.log(err)
+    });
