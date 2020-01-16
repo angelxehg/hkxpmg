1467a9b972066cf0bca3ffe0971a4fd322c6adf0
Angel Hurtado <angelxehg@outlook.com>
2020-01-16 15:59:31 -0600

    Add mongodb url

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 'use strict';
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://'), (err, res) => {
+mongoose.connect('mongodb://heroku_qqw6mhz5:7o0ug689i04qd2c7mdtdj1luf3@ds331568.mlab.com:31568/heroku_qqw6mhz5'), (err, res) => {
     if (err) {
         throw err;
     } else {
