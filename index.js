46afa2d943c729d1ad46ed732077834b8dbce8c9
Angel Hurtado <angelxehg@outlook.com>
2020-01-16 16:20:43 -0600

    comments

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
+/* Require packages */
 'use strict';
 var mongoose = require('mongoose');
+
+/* Connect database */
+console.log('Attempting to connect MongoDB')
 mongoose.connect('mongodb://heroku_qqw6mhz5:7o0ug689i04qd2c7mdtdj1luf3@ds331568.mlab.com:31568/heroku_qqw6mhz5', {
     useNewUrlParser: true,
     useUnifiedTopology: true
-}).then(() => console.log('DB Connected!'))
+}).then(() => console.log('MongoDB Connected!'))
     .catch(err => {
         console.log(err)
     });
