57dac49116c6021f91654b7f82adb465bcddab8f
Angel Hurtado <angelxehg@outlook.com>
2020-02-26 22:06:07 +0000

    Revert "Split into files"
    This reverts commit b3345a0c9d97d736d4ff1f12b36eeb3462d7e71d.

--- src/app.js
+++ src/app.js
@@ -1,18 +1,18 @@
-this.connect();
 
-function onMessageArrived(message) {
-    console.log(message.payloadString);
-    var json = $.parseJSON(message.payloadString);
-    if (json.type.localeCompare("heartbeat") == 0) {
-        online(json.time);
-    }
-    if (json.type.localeCompare("access") == 0) {
-        if (json.isKnow) {
-            createRowOn("denied_list", "Known access at " + convertTime(json.time))
-        } else {
-            createRowOn("allowed_list", "Unknown access at " + convertTime(json.time))
-        }
-    }
+client = new Paho.MQTT.Client("64.227.94.40", 8083, "");
+// set callback handlers
+client.onConnectionLost = onConnectionLost;
+client.onMessageArrived = onMessageArrived;
+// connect the client
+client.connect({ onSuccess: onConnect });
+// called when the client connects
+function onConnect() {
+    setBtnDanger("mqtt");
+    setBtnSuccess("mqtt");
+    // Once a connection has been made, make a subscription and send a message.
+    console.log("onConnect");
+
+    client.subscribe("proyecto/#");
 }
 
 function setBtnDanger(objectName) {
@@ -25,9 +25,51 @@ function setBtnSuccess(objectName) {
     document.getElementById(objectName).classList.add('btn-success');
 }
 
+// called when the client loses its connection
+function onConnectionLost(responseObject) {
+    if (responseObject.errorCode !== 0) {
+        console.log("onConnectionLost:" + responseObject.errorMessage);
+        setBtnSuccess("mqtt");
+        setBtnDanger("mqtt");
+    }
+}
+// called when a message arrives
+function onMessageArrived(message) {
+    console.log(message.payloadString);
+    var json = $.parseJSON(message.payloadString);
+
+    if (json.type.localeCompare("heartbeat") == 0) {
+        online(json.time);
+    }
+    if (json.type.localeCompare("access") == 0) {
+        if (json.isKnow) {
+            //Credencial Conocida
+            createRowOn("denied_list", "Known access at " + convertTime(json.time))
+        } else {
+            //Credencial Desconocida
+            createRowOn("allowed_list", "Unknown access at " + convertTime(json.time))
+        }
+    }
+
+
+}
+
 function createRowOn(element, message) {
     var list = document.getElementById(element);
     list.innerHTML += "<li class='list-group-item'>" + message + "</li>";
+}
+
+function convertTime(time) {
+    let unix_timestamp = time;
+    var date = new Date(unix_timestamp * 1000);
+    var Anio = date.getFullYear();
+    var Mes = date.getMonth();
+    var Dia = date.getDate();
+    var Hora = date.getHours();
+    var Minuto = "0" + date.getMinutes();
+    var Segundo = "0" + date.getSeconds();
+    var formattedTime = Anio + '-' + Mes + '-' + Dia + ' ' + Hora + ':' + Minuto.substr(-2) + ':' + Segundo.substr(-2);
+    return formattedTime;
 }
 
 function online(time) {
