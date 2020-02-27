ae4991184115f72e54654a4e13d292f9c3d0885f
Angel Hurtado <angelxehg@outlook.com>
2020-02-27 10:42:16 -0600

    Compare heartbeat

--- src/app.js
+++ src/app.js
@@ -1,3 +1,5 @@
+var lastHeartbeat = null;
+
 function connect() {
     client = new Paho.MQTT.Client("64.227.94.40", 8083, "");
     client.onConnectionLost = onConnectionLost;
@@ -5,9 +7,16 @@ function connect() {
     client.connect({ onSuccess: onConnect });
 }
 
+function sampleHeartbeat() {
+    message = new Paho.MQTT.Message('{"type":"heartbeat", "time":1582820388}');
+    message.destinationName = "proyecto";
+    client.send(message);
+}
+
 function onConnect() {
     statusOnHolding();
     client.subscribe("proyecto/#");
+    sampleHeartbeat();
 }
 
 function statusOnHolding() {
@@ -43,7 +52,7 @@ function onMessageArrived(message) {
     try {
         var json = $.parseJSON(message.payloadString);
         if (json.type.localeCompare("heartbeat") == 0) {
-            statusOnConnected();
+            lastHeartbeat = new Date();
         }
         if (json.type.localeCompare("access") == 0) {
             if (json.isKnow) {
@@ -75,4 +84,28 @@ function convertTime(time) {
     return formattedTime;
 }
 
+function formatedDate(date) {
+    var Anio = date.getFullYear();
+    var Mes = date.getMonth();
+    var Dia = date.getDate();
+    var Hora = date.getHours();
+    var Minuto = "0" + date.getMinutes();
+    var Segundo = "0" + date.getSeconds();
+    var formattedTime = Anio + '-' + Mes + '-' + Dia + ' ' + Hora + ':' + Minuto.substr(-2) + ':' + Segundo.substr(-2);
+    return formattedTime;
+}
+
+function checkDevice() {
+    var current = new Date();
+    var dif = current - this.lastHeartbeat;
+    console.log("Last heartbeat " + dif + "ms ago");
+    if (dif > 30000) {
+        statusOnHolding();
+    } else {
+        statusOnConnected();
+    }
+}
+
 this.connect();
+
+var daemon = setInterval(checkDevice, 5000);
